cb9cafee0a222b9bdb8168f3cf15d0d59184c9de
Georgi 🧡 <georgina.berrezel@gmail.com>
2025-06-18 11:43:13 +0200

    add ascending/descending sorting by price and date in the listings

--- src/app/components/annonce-list/annonce-list.component.ts
+++ src/app/components/annonce-list/annonce-list.component.ts
@@ -4,7 +4,6 @@ import { AnnonceService, Annonce } from '../../services/annonce.service';
 import { AnnonceCardComponent } from '../annonce-card/annonce-card.component';
 import { FormsModule } from '@angular/forms';
 
-
 @Component({
   selector: 'app-annonce-list',
   standalone: true,
@@ -12,30 +11,41 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './annonce-list.component.html',
   styleUrls: ['./annonce-list.component.scss']
 })
-  export class AnnonceListComponent implements OnInit {
-    annonces: Annonce[] = [];
+export class AnnonceListComponent implements OnInit {
+  annonces: Annonce[] = [];
 
-    villeFilter = '';
-    prixMaxFilter: number | null = null;
-    surfaceMinFilter: number | null = null;
-    dateMinFilter: string = '';
+  villeFilter = '';
+  prixMaxFilter: number | null = null;
+  surfaceMinFilter: number | null = null;
+  dateMinFilter = '';
 
-    constructor(private annonceService: AnnonceService) {}
+  sortBy: 'prix' | 'date' = 'prix';
+  sortDirection: 'asc' | 'desc' = 'asc';
 
-    ngOnInit(): void {
-      this.annonceService.getAnnonces().subscribe((data: Annonce[]) => {
-        this.annonces = data;
-      });
-    }
+  constructor(private annonceService: AnnonceService) {}
 
-    filteredAnnonces(): Annonce[] {
-      return this.annonces.filter(a => {
-        const matchVille = this.villeFilter ? a.ville.toLowerCase().includes(this.villeFilter.toLowerCase()) : true;
-        const matchPrix = this.prixMaxFilter !== null ? a.prix <= this.prixMaxFilter : true;
-        const matchSurface = this.surfaceMinFilter !== null ? a.surface >= this.surfaceMinFilter : true;
-        const matchDate = this.dateMinFilter ? new Date(a.datePublication) >= new Date(this.dateMinFilter) : true;
-
-        return matchVille && matchPrix && matchSurface && matchDate;
-      });
-    }
+  ngOnInit(): void {
+    this.annonceService.getAnnonces().subscribe((data: Annonce[]) => {
+      this.annonces = data;
+    });
   }
+
+  filteredAnnonces(): Annonce[] {
+    const filtered = this.annonces.filter(a => {
+      const matchVille = this.villeFilter ? a.ville.toLowerCase().includes(this.villeFilter.toLowerCase()) : true;
+      const matchPrix = this.prixMaxFilter !== null ? a.prix <= this.prixMaxFilter : true;
+      const matchSurface = this.surfaceMinFilter !== null ? a.surface >= this.surfaceMinFilter : true;
+      const matchDate = this.dateMinFilter ? new Date(a.datePublication) >= new Date(this.dateMinFilter) : true;
+      return matchVille && matchPrix && matchSurface && matchDate;
+    });
+
+    filtered.sort((a, b) => {
+      const valA = this.sortBy === 'prix' ? a.prix : new Date(a.datePublication).getTime();
+      const valB = this.sortBy === 'prix' ? b.prix : new Date(b.datePublication).getTime();
+
+      return this.sortDirection === 'asc' ? valA - valB : valB - valA;
+    });
+
+    return filtered;
+  }
+}
