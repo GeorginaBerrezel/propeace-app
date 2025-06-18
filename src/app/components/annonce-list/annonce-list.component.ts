02e2a3512dcc89725ca95716d7e489869550681a
Georgi 🧡 <georgina.berrezel@gmail.com>
2025-06-18 11:36:12 +0200

    add dynamic filters for city, max price, surface and date

--- src/app/components/annonce-list/annonce-list.component.ts
+++ src/app/components/annonce-list/annonce-list.component.ts
@@ -2,23 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnnonceService, Annonce } from '../../services/annonce.service';
 import { AnnonceCardComponent } from '../annonce-card/annonce-card.component';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-annonce-list',
   standalone: true,
-  imports: [CommonModule, AnnonceCardComponent],
+  imports: [CommonModule, FormsModule, AnnonceCardComponent],
   templateUrl: './annonce-list.component.html',
   styleUrls: ['./annonce-list.component.scss']
 })
-export class AnnonceListComponent implements OnInit {
-  annonces: Annonce[] = [];
+  export class AnnonceListComponent implements OnInit {
+    annonces: Annonce[] = [];
 
-  constructor(private annonceService: AnnonceService) {}
+    villeFilter = '';
+    prixMaxFilter: number | null = null;
+    surfaceMinFilter: number | null = null;
+    dateMinFilter: string = '';
 
-  ngOnInit(): void {
-    this.annonceService.getAnnonces().subscribe((data: Annonce[]) => {
-      console.log('Annonces reçues:', data);
-      this.annonces = data;
-    });
+    constructor(private annonceService: AnnonceService) {}
+
+    ngOnInit(): void {
+      this.annonceService.getAnnonces().subscribe((data: Annonce[]) => {
+        this.annonces = data;
+      });
+    }
+
+    filteredAnnonces(): Annonce[] {
+      return this.annonces.filter(a => {
+        const matchVille = this.villeFilter ? a.ville.toLowerCase().includes(this.villeFilter.toLowerCase()) : true;
+        const matchPrix = this.prixMaxFilter !== null ? a.prix <= this.prixMaxFilter : true;
+        const matchSurface = this.surfaceMinFilter !== null ? a.surface >= this.surfaceMinFilter : true;
+        const matchDate = this.dateMinFilter ? new Date(a.datePublication) >= new Date(this.dateMinFilter) : true;
+
+        return matchVille && matchPrix && matchSurface && matchDate;
+      });
+    }
   }
-}
